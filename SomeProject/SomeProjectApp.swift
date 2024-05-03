3b47e7aea56a9e652f75ba0396ed81c557949551
Mateus Rodrigues <mateus.fr26@gmail.com>
2024-05-03 20:44:20 -0300

    Initial commit

---
+++ SomeProject/SomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeProjectApp.swift
+//  SomeProject
+//
+//  Created by Mateus Rodrigues on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
